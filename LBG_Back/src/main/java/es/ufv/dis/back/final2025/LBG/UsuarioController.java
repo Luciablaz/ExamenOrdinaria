e720b5cbed8ed2ab286fb4816a961af8a4fe7b9b
Luciablaz <luciablazquezgallardo@gmail.com>
2025-06-21 19:42:42 +0200

    ClaseUsuarioController implementada

--- LBG_Back/src/main/java/es/ufv/dis/back/final2025/LBG/UsuarioController.java
+++ LBG_Back/src/main/java/es/ufv/dis/back/final2025/LBG/UsuarioController.java
@@ -1,4 +1,30 @@
 package es.ufv.dis.back.final2025.LBG;
 
+import org.springframework.web.bind.annotation.*;
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/usuarios")
 public class UsuarioController {
+
+    private final UsuarioService servicio;
+
+    public UsuarioController(UsuarioService servicio) {
+        this.servicio = servicio;
+    }
+
+    @GetMapping
+    public List<Usuario> getUsuarios() {
+        return servicio.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public Usuario getUsuario(@PathVariable String id) {
+        return servicio.findById(id);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteUsuario(@PathVariable String id) {
+        servicio.delete(id);
+    }
 }
