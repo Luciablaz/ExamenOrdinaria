2331f80d31a10f6cebb52a9df98d831a15365f7b
Luciablaz <luciablazquezgallardo@gmail.com>
2025-06-21 19:16:36 +0200

    ClaseLectorJson implementada

--- LBG_Back/src/main/java/es/ufv/dis/back/final2025/LBG/LectorJson.java
+++ LBG_Back/src/main/java/es/ufv/dis/back/final2025/LBG/LectorJson.java
@@ -1,4 +1,24 @@
 package es.ufv.dis.back.final2025.LBG;
 
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+
 public class LectorJson {
+    public ArrayList<Usuario> leeFicheroJson(){
+        try {
+            BufferedReader reader = new BufferedReader(
+                    new InputStreamReader(getClass().getClassLoader().getResourceAsStream("usuarios.json"))
+            );
+            ArrayList<Usuario> listaUsuario = new Gson().fromJson(reader, new TypeToken<ArrayList<Usuario>>() {}.getType());
+            reader.close();
+            return listaUsuario;
+        } catch (Exception ex) {
+            ex.printStackTrace();
+            return new ArrayList<>();
+        }
+    }
 }
