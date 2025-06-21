30623e8e8ddf3faaa67541e0b3f380db85a4f85e
Luciablaz <luciablazquezgallardo@gmail.com>
2025-06-21 19:02:51 +0200

    ClaseMetodoPago implementada

--- LBG_Back/src/main/java/es/ufv/dis/back/final2025/LBG/MetodoPago.java
+++ LBG_Back/src/main/java/es/ufv/dis/back/final2025/LBG/MetodoPago.java
@@ -1,4 +1,24 @@
 package es.ufv.dis.back.final2025.LBG;
 
 public class MetodoPago {
+    private long numeroTarjeta;
+    private String nombreAsociado;
+
+    // Getters y Setters
+
+    public long getNumeroTarjeta() {
+        return numeroTarjeta;
+    }
+
+    public void setNumeroTarjeta(long numeroTarjeta) {
+        this.numeroTarjeta = numeroTarjeta;
+    }
+
+    public String getNombreAsociado() {
+        return nombreAsociado;
+    }
+
+    public void setNombreAsociado(String nombreAsociado) {
+        this.nombreAsociado = nombreAsociado;
+    }
 }
