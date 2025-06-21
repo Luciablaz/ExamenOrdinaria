eedb1adcad90b296a95b51bad0b44732d33c5108
Luciablaz <luciablazquezgallardo@gmail.com>
2025-06-21 19:38:25 +0200

    ClaseUsuarioService implementada

--- LBG_Back/src/main/java/es/ufv/dis/back/final2025/LBG/UsuarioService.java
+++ LBG_Back/src/main/java/es/ufv/dis/back/final2025/LBG/UsuarioService.java
@@ -1,4 +1,29 @@
 package es.ufv.dis.back.final2025.LBG;
 
+import org.springframework.stereotype.Service;
+import java.util.List;
+
+@Service
 public class UsuarioService {
+
+    private List<Usuario> usuarios;
+
+    public UsuarioService() {
+        LectorJson lector = new LectorJson();
+        this.usuarios = lector.leeFicheroJson();
+    }
+
+    public List<Usuario> findAll() {
+        return usuarios;
+    }
+
+    public Usuario findById(String id) {
+        return usuarios.stream()
+                .filter(u -> u.getId().toString().equals(id))
+                .findFirst().orElse(null);
+    }
+
+    public void delete(String id) {
+        usuarios.removeIf(u -> u.getId().toString().equals(id));
+    }
 }
